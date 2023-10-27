8707ad422d192d0b5afa97ae635577f5add97b1d
virginie.bellec <vrgnbllc@gmail.com>
2023-10-27 10:45:34 +0200

    finished JsonReaderUtil class

--- src/main/java/com/safetynet/safetynetalerts/service/JsonReaderUtil.java
+++ src/main/java/com/safetynet/safetynetalerts/service/JsonReaderUtil.java
@@ -9,14 +9,23 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-public class Utils {
+public class JsonReaderUtil {
 
     void readJSON() throws IOException {
+        // Accessing a resource from the classpath using Spring
         Resource resource = new ClassPathResource("classpath:data.json");
+
+        // Loading the file from the folder source
         File file = resource.getFile();
+
+        // Reading file and storing all info, regardless of type, in 'content' array variable
         byte[] content = Files.readAllBytes(file.toPath());
+
+        // Converting array content into an iterator object/creating iterator from byte array
         JsonIterator iter = JsonIterator.parse(content);
-        Any any = JsonIterator.readAny();
+
+        // Binding iterator's next value into any-type object
+        Any any = iter.readAny();
 
     }
 
